f1ebd76b85411eab089a2260d36a213fb6f6626f
karliuka <wit.kharkov@gmail.com>
2016-12-09 20:28:52 +0200

    comment models

---
+++ Model/Config/Comment/Port.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Faonni
+ *  
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade module to newer
+ * versions in the future.
+ * 
+ * @package     Faonni_Smtp
+ * @copyright   Copyright (c) 2016 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace Faonni\Smtp\Model\Config\Comment;
+
+use Magento\Config\Model\Config\CommentInterface;
+
+/**
+ * System configuration comment model
+ */
+class Port implements CommentInterface
+{
+    /**
+     * Retrieve element comment by element value
+     * 
+     * @param string $elementValue
+     * @return string
+     */	
+	public function getCommentText($elementValue)
+    {
+        return __('Usually is 25, 587 or 465. Please consult with your service provider.');
+    }
+}
